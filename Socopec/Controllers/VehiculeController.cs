1fca2bba58714edcb97949d384396b89f106e2a3
Maxime Masetto <mmasetto@mtocake.com>
2020-02-24 14:07:19 +0100

    Ajout class

--- Socopec/Controllers/VehiculeController.cs
+++ Socopec/Controllers/VehiculeController.cs
@@ -9,16 +9,16 @@ namespace Socopec.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class SOCVehiculeController : ControllerBase
+    public class VehiculeController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "AW-689-HS", "ZF-239-HR", "RF-109-JJ", "OP-542-HH", "WW-767-GH", "WW-154-WW"
         };
 
-        private readonly ILogger<SOCVehiculeController> _logger;
+        private readonly ILogger<VehiculeController> _logger;
 
-        public SOCVehiculeController(ILogger<SOCVehiculeController> logger)
+        public VehiculeController(ILogger<VehiculeController> logger)
         {
             _logger = logger;
         }
@@ -27,10 +27,10 @@ namespace Socopec.Controllers
     public IEnumerable<Vehicule> Get()
     {
         var rng = new Random();
-        return Enumerable.Range(1, 1).Select(index => new Vehicule
+        return Enumerable.Range(1, 2).Select(index => new Vehicule
         {
-            VEHI_id = Convert.ToInt32(rng),
-            VEHI_DateFabrication = Convert.ToDateTime("20/09/2019"),
+            VEHI_id = 4,
+            VEHI_DateFabrication = DateTime.Today,
             VEHI_Immatriculation = Summaries[rng.Next(Summaries.Length)]    
         })
         .ToArray();
