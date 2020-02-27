219032cb4e33b1059d4097b3278a78a37aac9bae
Jon Zombie Bootcamp <mmasetto@mtohosting.com>
2020-02-27 15:23:10 +0100

    Agences fonctionnel

---
+++ Socopec/Controllers/AgenceController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Socopec.Models;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Socopec.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AgenceController : ControllerBase
+    {
+
+        private readonly ILogger<VehiculeController> _logger;
+
+        public AgenceController(ILogger<VehiculeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public void GetDataFromDB()
+        {
+
+
+        }
+
+        [HttpGet]
+        public IEnumerable GetList()
+        {
+            using (socopecContext db = new socopecContext())
+            {
+
+                var query = from Agence in db.Set<Agences>()
+                            
+                            select new
+                            {
+                                Agence
+                            };
+
+
+                return query.ToList();
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
